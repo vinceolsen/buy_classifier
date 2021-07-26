cb4eb1e9bf2fea1bed9e523bf2535796601b86ae
rickpdx <59929817+rickpdx@users.noreply.github.com>
2021-07-26 10:31:00 -0700

    Update to ProcessInput class. Addition of visuals.py

--- src/main.py
+++ src/main.py
@@ -12,66 +12,24 @@ from matplotlib import pyplot as plt
 from sklearn.metrics import confusion_matrix
 from process_input import ProcessInput
 
-
-def process_datasets(src, btc_src):
-
-    # Read BTC-Historical-Price
-    btc_df = pd.read_csv(btc_src, sep=',', names=[
-                         "Date", "Price", "Open", "High", "Low", "Vol", "Change"])
-    print('Initial shape', btc_df.shape)
-
-    # Read 1st dataset and convert to numpy
-    ds_df = pd.read_csv(src[0])
-
-    # Filter BTC dataframe by Date
-    # btc_df will match the dates of M-F market datasets
-    btc_df = btc_df.set_index(['Date'])
-    temp = ds_df.set_index(['Date'])
-    btc_df = btc_df[btc_df.index.isin(temp.index)].reset_index()
-    print('Resulting shape', btc_df.shape)
-
-    # OPTION 1: DROP Columns so dataset features can match
-    # btc_df.drop(['Price', 'Change'], axis=1)
-
-    # BTC prices and Dataset features don't match yet so we don't stack
-    # btc = btc_df.to_numpy()
-    dataset = ds_df.to_numpy()
-    # dataset = np.dstack((btc, dataset))
-
-    # Read remaining datasets and stack them along the 3rd axis
-    for set in src[1:]:
-        df = pd.read_csv(set)
-        data = df.to_numpy()
-        dataset = np.dstack((dataset, data))
-    print('Loaded datasets shape:', dataset.shape)
-
-    return dataset
-
-
 if __name__ == "__main__":
 
     # Set up for Windows and Linux
     dataset_folder = Path("raw_data")
     preprocessed_folder = Path("preprocessed_data")
 
-    # Process BTC historical data
-    infile = dataset_folder / 'BTC-Historical-Price.csv'
-    outfile = preprocessed_folder / 'BTC-Historical-Price.csv'
-    PI = ProcessInput(infile, outfile)
-    PI.read_btc_historical_csv()
-    PI.write_btc_historical_csv()
+    PI = ProcessInput(dataset_folder, preprocessed_folder)
 
-    # Read in datasets file names
-    src_in = dataset_folder / "datasets.txt"
-    with open(src_in, 'r') as file:
-        data_string = file.read().replace('\n', '')
-    data_string = data_string.split(",")
-    src_list = [dataset_folder / x for x in data_string]
+    # Run process_datasets() first to save new csv files
+    # If csv files already exist, then use read_preprocessed data
 
-    # Setup data
-    # TODO
-    # Temporarily only using the first 4 datasets
-    dataset = process_datasets(src_list[0:4], outfile)
+    # Process datasets
+    PI.process_datasets()
+
+    # Load datasets from preprocessed_data
+    PI.read_preprocessed_data()
+
+    # TODO Split dataset in training and testing sets
 
     # Setup model
     # TODO Figure out what model to start with. May need to be one we make from scratch
--- src/process_input.py
+++ src/process_input.py
@@ -5,6 +5,9 @@ Crypto buy/sell indicators project
 import calendar
 import csv
 import datetime
+import pandas as pd
+import numpy as np
+from pathlib import Path
 
 
 class ProcessInput:
@@ -18,55 +21,171 @@ class ProcessInput:
         self.infile = infile
         self.outfile = outfile
         self.headers = None
-        self.btc_historical_data = []
+        self.btc_data = []
+        self.dataset_n = 3
+        self.src_list = []
+        self.dataset = []
+        self.labels = []
+        self.length = None
 
-    def read_btc_historical_csv(self):
-        """ Read the BTC historical data and process it
+    def read_dataset_list(self):
+        """
+        Read in datasets names from datasets.txt
+        """
+        src_in = self.infile / "datasets.txt"
+        with open(src_in, 'r') as file:
+            data_string = file.read().replace('\n', '')
+        self.data_string = data_string.split(",")
+        self.src_list = [self.infile / x for x in self.data_string]
+
+    def write_to_csv(self, df, index, visual=False):
+        """ Write the processed historical data back to file
 
         :return: None
         """
-        with open(self.infile) as f:
-            # First line is the headers
-            self.headers = f.readline()
+        if visual == True:
+            visual_dir = Path('visualizations')
+            dir = visual_dir / self.data_string[index]
+            df.to_csv(dir, index=False)
 
-            reader = csv.reader(f, delimiter=',', quotechar='"')
-            for row in reader:
-                # remove commas from the elements
-                for val in range(len(row)):
-                    row[val] = row[val].replace(',', "")
+        dir = self.outfile / self.data_string[index]
+        df.to_csv(dir, index=False)
 
-                # Process date
-                d = row[0].split(' ')
-                month = d[0]
-                months = {month: index for index, month in enumerate(
-                    calendar.month_abbr) if month}
+    def read_preprocessed_data(self, index=None):
+        """
+        Read in one or all of the datasets from preprocessed_data 
+        directory
+        :params: Provide specific index using datasets.txt
+        """
+        self.read_dataset_list()
+        n = 10
+        labels = []
+        path_list = [self.outfile / x for x in self.data_string]
 
-                row[0] = datetime.date(int(d[2]), months[month], int(d[1]))
+        if index is None:
+            for name in path_list[:self.dataset_n]:
+                df = pd.read_csv(name)
 
-                # Convert all numerical values to floats
-                row[1] = float(row[1])
-                row[2] = float(row[2])
-                row[3] = float(row[3])
-                row[4] = float(row[4])
+                # Store shape of dataset
+                if self.length is None:
+                    self.length = df.shape[0]
 
-                # Process trading volume
-                if row[5][-1] == 'K':
-                    row[5] = float(row[5][:-1]) * 1000
-                elif row[5][-1] == 'M':
-                    row[5] = float(row[5][:-1]) * 1000000
+                # Generate labels
+                labels = self.generate_labels(df, n, labels)
 
-                # Remove percent sign from last element
-                row[6] = float(row[6][:-1])
+                # Stack data along the 3rd axis
+                data = df.to_numpy()
+                self.dataset = np.dstack((self.dataset, data))
 
-                self.btc_historical_data.insert(0, row)
+        else:
+            df = pd.read_csv(path_list[index])
+            self.length = df.shape[0]
 
-    def write_btc_historical_csv(self):
-        """ Write the processed BTC historical data back to file
+            # Generate labels
+            labels = self.generate_labels(df, n, labels)
+            self.dataset = df.to_numpy()
 
+        self.labels = labels
+
+    def align_initial_dataframes(self):
+        """
+        Aligns axes of first two datasets for stacking 
+        :return: bitcoin price df and df of next dataset on list
+        """
+        # Read BTC-Historical-Price
+        btc_df = pd.read_csv(self.src_list[0])
+
+        # Read 1st US Exchange dataset
+        ds_df = pd.read_csv(self.src_list[1])
+
+        # Drop null values from the price
+        btc_df = btc_df.dropna()
+
+        # Drop holidays and weekend from BTC price data
+        btc_df = self.drop_dates(btc_df, ds_df)
+
+        # Drop dates from financial data where BTC was null
+        ds_df = self.drop_dates(ds_df, btc_df)
+
+        # Save csv with dates
+        self.write_to_csv(btc_df, 0, visual=True)
+
+        # Drop dates column
+        btc_df = btc_df.drop(['Date'], axis=1)
+        self.write_to_csv(btc_df, 0)
+        self.length = ds_df.shape[0]
+
+        return ds_df
+
+    def drop_dates(self, df1, df2_ref):
+        """
+        Drop dates for crypto historical price data
+        :return: dataframe
+        """
+        df1 = df1.set_index(['Date'])
+        temp = df2_ref.set_index(['Date'])
+        df1 = df1[df1.index.isin(temp.index)].reset_index()
+        return df1
+
+    def process_datasets(self):
+        """
+        Process datasets and stack along 3rd access
         :return: None
         """
-        with open(self.outfile, 'w', newline='') as f:
-            writer = csv.writer(
-                f, delimiter=',', quotechar='|', quoting=csv.QUOTE_MINIMAL)
-            for row in self.btc_historical_data:
-                writer.writerow(row)
+        self.read_dataset_list()
+        ds_df = self.align_initial_dataframes()
+
+        num = 2
+        for set in self.src_list[2:self.dataset_n]:
+            df = pd.read_csv(set)
+
+            # Prepare data
+            df = self.drop_dates(df, ds_df)
+            df = df.drop(['Date'], axis=1)
+            self.write_to_csv(df, num)
+            num += 1
+
+        ds_df = ds_df.drop(['Date'], axis=1)
+        self.write_to_csv(ds_df, 1)
+
+    def generate_labels(self, df, n, labels=None):
+        """
+        Generates labels for a dataset
+        :params: dataframe, size of series, existing labels (default is none)
+        :return: labels
+        """
+
+        threshold = .01
+        if labels == None:
+            labels = []
+
+        prices = df['Open']
+        prices = prices.to_numpy()
+
+        row_num = 0
+        for row in prices:
+            if row_num + n >= self.length:
+                series = prices[row_num:self.length+1]
+
+            else:
+                series = prices[row_num:n+row_num+1]
+
+            val = self.calculate_change(row, series)
+            labels.append(1) if val >= threshold else labels.append(0)
+            row_num += 1
+
+        return labels
+
+    def calculate_change(self, price, series):
+        """
+        Compare a price to the max value of a list of prices
+        :return: resulting value or 
+        0 if price is the same or less than max value.
+        """
+        max_val = np.amax(series)
+
+        if price < max_val:
+            change = (max_val - price) / price
+            return change
+        else:
+            return 0
