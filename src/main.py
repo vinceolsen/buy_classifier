65f006092e84f7ec58dc3e82ba7d1a5cdbd93507
David Hawbaker <djhawbaker@gmail.com>
2021-07-31 09:40:18 -0700

    Add more variables instead of hard coded values. Using the indices passed in to the experiment function now. Started adding comment blocks. Cleaned up old code.

--- src/main.py
+++ src/main.py
@@ -19,6 +19,13 @@ from process_input import ProcessInput
 # Update the sys.path to search in the python project directory
 
 def create_model(n_timesteps, n_features, n_outputs):
+    """
+
+    :param n_timesteps:
+    :param n_features:
+    :param n_outputs:
+    :return:
+    """
     verbose, epochs, batch_size = 0, 10, 32
     #n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
     model = models.Sequential()
@@ -33,43 +40,42 @@ def create_model(n_timesteps, n_features, n_outputs):
 
     model.summary()
     return model
-    # fit network
-    #model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=verbose)
-    # evaluate model
-    #_, accuracy = model.evaluate(testX, testy, batch_size=batch_size, verbose=0)
-    #return accuracy
 
 
-# summarize scores
 def summarize_results(scores):
+    """
+
+    :param scores:
+    :return:
+    """
     print(scores)
     m, s = np.mean(scores), np.std(scores)
     print('Accuracy: %.3f%% (+/-%.3f)' % (m, s))
 
 
-# run an experiment
-def run_experiment(dataset, epochs=10):
-    # load data
-    # trainX, trainy, testX, testy = split_data(dataset)
-    # repeat experiment
-    scores = list()
-    for r in range(epochs):
-        score = evaluate_model(trainX, trainy, testX, testy)
-        score = score * 100.0
-        print('>#%d: %.3f' % (r + 1, score))
-        scores.append(score)
-    # summarize results
-    summarize_results(scores)
+def run_experiment(model, dataset, labels, training_indices, testing_indices, validation_indices, history_length,
+                   epochs=10):
+    """
 
-
-def run_experiment2(model, dataset, labels, training_indices, testing_indices, validation_indices, history_length,
-                    epochs=10):
-    # this is just an example of how to iterate over the datasets, this code does not work
-    # TODO: get the training, testing, and validating to work
-    scores = list()
+    :param model: The ML Model
+    :param dataset: The dataset to use
+    :param labels: The labels for the data
+    :param training_indices: List of indices for which data points should be trained on
+    :param testing_indices: List of indices for which data points should be tested on
+    :param validation_indices: List of indices for which data points should be validated on
+    :param history_length: The number of days to include in the history chunk
+    :param epochs: How many times should the ML algorithm be trained
+    :return: None
+    """
     # Add shape to the arrays
-    chunks = np.empty([1717, 506, 5, 4])
-    chunk_labels = np.empty([1717, 1])
+    n_attributes = dataset.shape[2]
+    # TODO change the 5 to a variable. It comes from the 1:6 slicing in creating chunks below
+    chunks = np.empty([len(training_indices), history_length, 5, n_attributes])
+    chunk_labels = np.empty([len(training_indices), 1])
+    test_chunks = np.empty([len(testing_indices), history_length, 5, n_attributes])
+    test_chunk_labels = np.empty([len(testing_indices), 1])
+    val_chunks = np.empty([len(validation_indices), history_length, 5, n_attributes])
+    val_chunk_labels = np.empty([len(validation_indices), 1])
 
     # Prep Data
     i = 0
@@ -77,53 +83,58 @@ def run_experiment2(model, dataset, labels, training_indices, testing_indices, v
         day = int(day)
         chunk = dataset[day - history_length:day, 1:6]
         label = labels[day, 1]
-        # convert values to 'float32' the model complains without it
+        # convert values to 'float32' the model complains without it TODO Verify this doesn't ruin the data
         c = np.asarray(chunk).astype(('float32'))
         chunks[i] = c
         chunk_labels[i] = label
         i += 1
 
-    total_train_vals = len(chunks)
-    eighty_percent = int(total_train_vals * 0.8)
-    ninety_percent = int(total_train_vals * 0.9)
-
     # Train model
-    history = model.fit(chunks[:eighty_percent], chunk_labels[:eighty_percent], epochs=10, shuffle=False)  # , callbacks=my_callbacks)
+    history = model.fit(chunks, chunk_labels, epochs=epochs, shuffle=False)  # , callbacks=my_callbacks)
     print(history)
 
-    eval_loss = model.evaluate(chunks[eighty_percent:ninety_percent], chunk_labels[eighty_percent:ninety_percent])
+    j = 0
+    for day in testing_indices:
+        day = int(day)
+        chunk = dataset[day - history_length:day, 1:6]
+        label = labels[day, 1]
+        # convert values to 'float32' the model complains without it TODO Verify this doesn't ruin the data
+        c = np.asarray(chunk).astype(('float32'))
+        test_chunks[j] = c
+        test_chunk_labels[j] = label
+        j += 1
+
+    k = 0
+    for day in validation_indices:
+        day = int(day)
+        chunk = dataset[day - history_length:day, 1:6]
+        label = labels[day, 1]
+        # convert values to 'float32' the model complains without it TODO Verify this doesn't ruin the data
+        c = np.asarray(chunk).astype(('float32'))
+        val_chunks[k] = c
+        val_chunk_labels[k] = label
+        k += 1
+
+    eval_loss = model.evaluate(test_chunks, test_chunk_labels)
     print(eval_loss)
 
     predictions = np.array([])
-    for x in chunks[ninety_percent:]:
-        predictions = np.concatenate([predictions, np.argmax(model.predict(x), axis=-1)])
+    for x in val_chunks:
+        predictions = np.concatenate(
+            [predictions, np.argmax(model.predict(x.reshape(1, history_length, 5, n_attributes)), axis=-1)])
 
-    print(confusion_matrix(labels, predictions))
-        # training
-        #correct, incorrect = 0, 0
+    print(confusion_matrix(val_chunk_labels, predictions))
 
-
-"""
-            prediction = model.predict(c.reshape(1, history_length, 5, 4))
-            if prediction == label:
-                correct += 1
-            else:
-                incorrect += 1
-        score = (correct / (correct + incorrect)) * 100
-        print('>#%d: %.3f' % (r + 1, score))
-        scores.append(score)
-    # summarize results
-    summarize_results(scores)
-
-"""
 
 if __name__ == "__main__":
     # Set up for Windows and Linux
     dataset_folder = Path("raw_data")
     preprocessed_folder = Path("preprocessed_data")
 
-    # You can process up to 5 datasets
     history_length = 506
+    epochs = 100
+
+    # You can process up to 5 datasets
     PI = ProcessInput(dataset_folder, preprocessed_folder,
                       max_buy_holding_period=10, num_of_securtities=3, target_roi=0.01, history_length=history_length)
 
@@ -140,33 +151,5 @@ if __name__ == "__main__":
     dataset, labels, training_indices, testing_indices, validation_indices = PI.get_data(.80, .10, .10)
 
     model = create_model(history_length, 5, 1)
-    run_experiment2(model, dataset, labels, training_indices, testing_indices, validation_indices, history_length)
-
-    # Setup model
-
-    # Train
-    # TODO what optimizer should be used?
-    """
-    opt = tf.keras.optimizers.Adam(learning_rate=0.01)
-    model.trainable = True
-    model.compile(loss='binary_crossentropy', optimizer=opt)
-    """
-
-    # TODO what value should training watch? loss?
-    """
-    my_callbacks = [
-        tf.keras.callbacks.EarlyStopping(monitor='loss')
-    ]
-    history = model.fit(trainer, validation_data=validator, epochs=10, shuffle=False, callbacks=my_callbacks)
-    print(history)
-
-    eval_loss = model.evaluate(validator)
-    print(eval_loss)
-
-    predictions = np.array([])
-    for x, y in validator:
-        predictions = np.concatenate([predictions, np.argmax(model.predict(x), axis=-1)])
-
-    print("part ii")
-    print(confusion_matrix(labels, predictions))
-    """
+    run_experiment(model, dataset, labels, training_indices, testing_indices, validation_indices, history_length,
+                   epochs=epochs)
