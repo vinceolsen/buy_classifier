deac27a826c0264799bc76fb4b7edd281384c60d
David Hawbaker <djhawbaker@gmail.com>
2021-08-02 17:44:13 -0700

    Added more steps per epoch. Added some testing and comments

--- src/main.py
+++ src/main.py
@@ -21,7 +21,7 @@ from process_input import ProcessInput
 def create_model(n_timesteps, n_features, n_outputs):
     """
 
-    :param n_timesteps:
+    :param n_timesteps: The number of days to
     :param n_features:
     :param n_outputs:
     :return:
@@ -29,14 +29,18 @@ def create_model(n_timesteps, n_features, n_outputs):
     verbose, epochs, batch_size = 0, 10, 32
     #n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
     model = models.Sequential()
+    #model.add(layers.Conv1D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps, 4)))
     model.add(layers.Conv2D(filters=64, kernel_size=3, activation='relu', input_shape=(n_timesteps, n_features, 4)))
+    # TODO verify kernel_size for this
+    #model.add(layers.Conv1D(filters=64, kernel_size=1, activation='relu'))
     model.add(layers.Conv2D(filters=64, kernel_size=1, activation='relu'))
     model.add(layers.Dropout(0.5))
     model.add(layers.MaxPooling2D(pool_size=2))
     model.add(layers.Flatten())
     model.add(layers.Dense(100, activation='relu'))
     model.add(layers.Dense(n_outputs, activation='softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    opt = tf.keras.optimizers.Adam(learning_rate=0.01)
+    model.compile(loss='binary_crossentropy', optimizer=opt, metrics=['accuracy'])
 
     model.summary()
     return model
@@ -90,7 +94,7 @@ def run_experiment(model, dataset, labels, training_indices, testing_indices, va
         i += 1
 
     # Train model
-    history = model.fit(chunks, chunk_labels, epochs=epochs, shuffle=False)  # , callbacks=my_callbacks)
+    history = model.fit(chunks, chunk_labels, epochs=epochs, steps_per_epoch=128, shuffle=False)  # , callbacks=my_callbacks)
     print(history)
 
     j = 0
@@ -123,8 +127,13 @@ def run_experiment(model, dataset, labels, training_indices, testing_indices, va
         predictions = np.concatenate(
             [predictions, np.argmax(model.predict(x.reshape(1, history_length, 5, n_attributes)), axis=-1)])
 
-    print(confusion_matrix(val_chunk_labels, predictions))
 
+    # TODO
+    print(confusion_matrix(val_chunk_labels, predictions, labels='y_true'))
+
+    print("Train: Number of buy signals: ", list(chunk_labels.flatten()).count(1))
+    print("Test Number of buy signals: ", list(test_chunk_labels.flatten()).count(1))
+    print("Val Number of buy signals: ", list(val_chunk_labels.flatten()).count(1))
 
 if __name__ == "__main__":
     # Set up for Windows and Linux
@@ -132,7 +141,7 @@ if __name__ == "__main__":
     preprocessed_folder = Path("preprocessed_data")
 
     history_length = 506
-    epochs = 100
+    epochs = 40
 
     # You can process up to 5 datasets
     PI = ProcessInput(dataset_folder, preprocessed_folder,
